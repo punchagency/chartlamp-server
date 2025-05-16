0b45a77650289f7083d54421e198e7c41421ac1f
veyron-davids <abiolaajileye@gmail.com>
2025-05-16 02:29:06 +0100

    feat: adding docker && k8s && terraform

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import dotenv from "dotenv";
 import cron from "node-cron";
 
-  dotenv.config();
+dotenv.config();
 
-import { ExpressAdapter } from "@bull-board/express";
-import { createBullBoard } from "@bull-board/api";
-import { BullMQAdapter } from "@bull-board/api/bullMQAdapter";
+// import { ExpressAdapter } from "@bull-board/express";
+// import { createBullBoard } from "@bull-board/api";
+// import { BullMQAdapter } from "@bull-board/api/bullMQAdapter";
 
 import axios from "axios";
 import compression from "compression";
@@ -21,8 +21,8 @@ import api from "./routes";
 import swaggerDocument from "./swagger/swagger.json";
 import corsOptions from "./utils/corsOption";
 import { connectToMongo } from "./utils/mongo";
-import { allQueues } from "./utils/queue/index";
-import { pdfTextExtractorQueue } from "./utils/queue/pdfExtractor/producer";
+// import { allQueues } from "./utils/queue/index";
+// import { pdfTextExtractorQueue } from "./utils/queue/pdfExtractor/producer";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -41,17 +41,17 @@ app.get("/", (_req: Request, res: Response): Response => {
   return res.status(200).send("Chartlamp server 🤟");
 });
 
-const serverAdapter = new ExpressAdapter();
-serverAdapter.setBasePath("/admin/queues");
+// const serverAdapter = new ExpressAdapter();
+// serverAdapter.setBasePath("/admin/queues");
 
-createBullBoard({
-  queues: [...allQueues, pdfTextExtractorQueue].map(
-    (q) => new BullMQAdapter(q)
-  ),
-  serverAdapter,
-});
+// createBullBoard({
+//   queues: [...allQueues, pdfTextExtractorQueue].map(
+//     (q) => new BullMQAdapter(q)
+//   ),
+//   serverAdapter,
+// });
 
-app.use("/admin/queues", serverAdapter.getRouter());
+// app.use("/admin/queues", serverAdapter.getRouter());
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use("/api/v1", api);
@@ -83,7 +83,7 @@ const start = async (): Promise<void> => {
     await connectToMongo();
     app.listen(PORT, () => {
       console.log("RUN_WORKERS", process.env.RUN_WORKERS);
-      console.log(`Server started on port ${PORT}!!!!!!!`);
+      console.log(`Server started on port ${PORT}!!`);
     });
   } catch (error) {
     // console.error(error);
